009cabe8ab4ccc3d37bca416b3a42fba7cc2ceb0
Alice <alice.aga4nova@gmail.com>
2024-10-27 17:20:05 +0300

    занятие 2 примеры команды print c sep и end

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-str_1=input()
+# str_1=input() одиночный комментарий(одной строки)
+'''
 str_2=input()
 str_3=input()
-print(str_3)
+print(str_3) комментарий блока кода
 print(str_2)
 print(str_1)
+'''
+
+# использование sep и end
+print("apple", "banana", "cherry", sep=", ", end=".\n")
+print("apple", "banana", "cherry", sep="| ")
+print("apple", "banana", "cherry", sep="\n")
+
+
+'''
+условия задачи
+'''
+# код задачи
